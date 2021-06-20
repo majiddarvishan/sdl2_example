9aceaab59186b2941c0df6169420478ae4e6d6a5
Majid Darvishan <majiddarvishan@outlook.com>
2021-06-20 23:09:39 +0430

    Add playerControl

---
+++ playerControl.h
@@ -0,0 +1,66 @@
+#pragma once
+
+extern "C"
+{
+#include "helper.h"
+}
+
+
+class playerControl
+{
+public:
+    playerControl() = default;
+    virtual ~playerControl() = default;
+
+    void init();
+    void start();
+    void stop();
+
+private:
+    void assets_in();
+    void assets_out();
+    void render_value();
+    void get_value();
+
+private:
+    SDL_Surface* temp_surface = NULL;
+
+    SDL_Texture* bg = NULL;
+    SDL_Rect 	bg_dst;
+
+    SDL_Texture* slot = NULL;
+    SDL_Rect 	slot_dst;
+
+    SDL_Texture* fillbar = NULL;
+    SDL_Rect 	fillbar_dst;
+
+    SDL_Texture* button = NULL;
+    SDL_Rect 	button_dst;
+
+    SDL_Texture* shadow_top = NULL;
+    SDL_Rect 	shadow_top_dst;
+
+    SDL_Texture* shadow_bot = NULL;
+    SDL_Rect 	shadow_bot_dst;
+
+    SDL_Rect 	button_bounds_dst;
+
+    TTF_Font* font = NULL;
+    SDL_Color color = { 255,0,0,255 };
+    SDL_Texture* text = NULL;
+    SDL_Rect 	text_dst;
+
+    int value;
+    char string[4];
+
+    SDL_Point	mouse;
+    SDL_bool 	mouse_follow = SDL_FALSE;
+    SDL_Point 	mouse_offset;
+
+    SDL_Window* Window = NULL;
+    SDL_Renderer* Renderer = NULL;
+
+    int ww;
+    int wh;
+};
+
